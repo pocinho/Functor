9b491534a275efb407f9640e41dd3e67012003cd
Paulo Pocinho <pocinho@gmail.com>
2026-09-11 14:27:52 +0100

    added a cleanup script; updated and consolidated nuget references; temporarily disabled stale projects from the main solution file Functor.slnx

---
+++ build/cleanup.ps1
@@ -0,0 +1,12 @@
+﻿### cleanup.ps1 ###
+
+# Root path:
+$rootProjectPath = "..\src\"
+
+# File and directory name patterns that are safe to delete:
+$filenamePattern = @("bin", "obj", ".vs", "*.user", "*.bak", "*.old")
+
+# Clean project built artifacts and stale files:
+Get-ChildItem $rootProjectPath -Recurse -Force -Include $filenamePattern | Remove-Item -Recurse -Force -WhatIf
+
+### cleanup.ps1 ###
